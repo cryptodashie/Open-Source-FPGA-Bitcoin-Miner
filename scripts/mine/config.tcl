4666817c63ad896f2bc81cf1f004402d4088c141
Hacker Fantastic <matthew@myhackerhouse.com>
2018-03-19 10:45:06 +0000

    add miner configuration

---
+++ scripts/mine/config.tcl
@@ -0,0 +1,6 @@
+# Edit this with your details and save as config.tcl
+# They are the same worker server, user/pass you use
+# with other mining software.
+#
+set url "http://mine.p2pool.com:9332"
+set userpass "36PUbyPu1M7tuWsJvmVd3qFN6GYySginUM:anything"
